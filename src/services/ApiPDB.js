c69d729fbd0fe76a424ec9e5b05f7db0626707ca
j-asafe <kaua.mota2@etec.sp.gov.br>
2025-09-15 22:38:34 -0300

    ApiPDB.js

---
+++ src/services/ApiPDB.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://github.com/j-asafe/AppPlanetsDB.git"; // <-- mudar aqui para sua API 
+ 
+// Função para listar todos os itens
+async function getItems() {
+  const res = await fetch(`${BASE_URL}items`); // end point que ira utilizar
+  const data = await res.json();
+  return data;
+}
+ 
+// Função para pegar detalhes de um item
+async function getItemById(id) {
+  const res = await fetch(`${BASE_URL}items/${id}`);
+  return await res.json();
+}
+ 
+export default { getItems, getItemById };
